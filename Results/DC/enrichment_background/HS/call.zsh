99e2c6ec6645c15f0ada772db15a5ac1207cc9f9
eduardo <eggduzao@gmail.com>
2019-08-14 07:48:14 +0200

    a

--- Results/DC/enrichment_background/HS/call.zsh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-bsub -J "BB" -o "BB_out.txt" -e "BB_err.txt" -W 24:00 -M 60000 -S 100 -P izkf -R "select[hpcwork]" ./pipeline.zsh
-
-
--- Results/IRF8_PU1/PerfectMatching/createTable_pipeline.zsh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-export PATH=$PATH:/hpcwork/izkf/bin/:/hpcwork/izkf/opt/bin/:/work/eg474423/eg474423_Projects/trunk/TfbsPrediction/exp/bin/
-export PYTHONPATH=$PYTHONPATH:/hpcwork/izkf/lib64/python2.6/:/hpcwork/izkf/lib64/python2.6/site-packages/:/hpcwork/izkf/lib/python2.6/:/hpcwork/izkf/lib/python2.6/site-packages/:/hpcwork/izkf/opt/lib64/python2.6/:/hpcwork/izkf/opt/lib64/python2.6/site-packages/:/hpcwork/izkf/opt/lib/python2.6/:/hpcwork/izkf/opt/lib/python2.6/site-packages/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/hpcwork/izkf/lib64/:/hpcwork/izkf/lib/:/hpcwork/izkf/opt/lib/:/hpcwork/izkf/opt/lib64/:/hpcwork/izkf/opt/lib/
-
-python /work/eg474423/eg474423_Projects/trunk/DendriticCells/PerfectMatching/createTable.py $1 $2
-
-
